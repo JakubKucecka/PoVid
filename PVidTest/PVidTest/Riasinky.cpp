682b46e9f7678a1a33331d09476f5b90c3b67fee
JakubKucecka <jakubkucecka@gmail.com>
2022-03-05 11:32:35 +0100

    Update cpp file for assigment1

--- PVidTest/PVidTest/Riasinky.cpp
+++ PVidTest/PVidTest/Riasinky.cpp
@@ -9,63 +9,65 @@ using namespace std;
 // without filters
 int main(int argc, char** argv)
 {
-	Mat image = imread("../../Assigment_1/Tv7.png", IMREAD_GRAYSCALE);
+	// nacitanie obrazka
+	Mat image = imread("../../Assigment_1/FullCeils/Tv7.png", IMREAD_GRAYSCALE);
 
+	// binarizacia
 	Mat thresholded;
 	threshold(image, thresholded, 90, 255, THRESH_BINARY);
-	imwrite("../../Assigment_1/Tv7FThresholded.png", thresholded);
+	imwrite("../../Assigment_1/FullCeils/Tv7FThresholded.png", thresholded);
 
 	// dilatacia
 	Mat dilatedF;
 	auto elipse_kernel = getStructuringElement(MORPH_ELLIPSE, Size(2, 2));
 	dilate(thresholded, dilatedF, elipse_kernel);
-	imwrite("../../Assigment_1/Tv7FDilate1.png", dilatedF);
+	imwrite("../../Assigment_1/FullCeils/Tv7FDilate1.png", dilatedF);
 
 	// closing
 	Mat closing;
 	elipse_kernel = getStructuringElement(MORPH_ELLIPSE, Size(7, 7));
 	erode(dilatedF, closing, elipse_kernel);
 	dilate(closing, closing, elipse_kernel);
-	imwrite("../../Assigment_1/Tv7FClosing.png", closing);
+	imwrite("../../Assigment_1/FullCeils/Tv7FClosing.png", closing);
 
 	// dilatacia
 	Mat dilatedS;
 	elipse_kernel = getStructuringElement(MORPH_ELLIPSE, Size(4, 4));
 	dilate(closing, dilatedS, elipse_kernel);
-	imwrite("../../Assigment_1/Tv7FDilate2.png", dilatedS);
+	imwrite("../../Assigment_1/FullCeils/Tv7FDilate2.png", dilatedS);
 
 	// closing
 	Mat closing2;
 	elipse_kernel = getStructuringElement(MORPH_ELLIPSE, Size(47, 47));
 	erode(dilatedS, closing2, elipse_kernel);
 	dilate(closing2, closing2, elipse_kernel);
-	imwrite("../../Assigment_1/Tv7FClosing2.png", closing2);
+	imwrite("../../Assigment_1/FullCeils/Tv7FClosing2.png", closing2);
 
 	// dilatacia
 	Mat dilatedT;
 	elipse_kernel = getStructuringElement(MORPH_ELLIPSE, Size(5, 5));
 	dilate(closing2, dilatedT, elipse_kernel);
-	imwrite("../../Assigment_1/Tv7FDilate3.png", dilatedT);
+	imwrite("../../Assigment_1/FullCeils/Tv7FDilate3.png", dilatedT);
 
 	// oppening
 	Mat opening;
 	elipse_kernel = getStructuringElement(MORPH_ELLIPSE, Size(32, 32));
 	dilate(dilatedT, opening, elipse_kernel);
 	erode(opening, opening, elipse_kernel);
-	imwrite("../../Assigment_1/Tv7FOpenning.png", opening);
+	imwrite("../../Assigment_1/FullCeils/Tv7FOpenning.png", opening);
 
 	// dilatacia
 	Mat dilated;
 	elipse_kernel = getStructuringElement(MORPH_ELLIPSE, Size(3, 3));
 	dilate(opening, dilated, elipse_kernel);
-	imwrite("../../Assigment_1/Tv7FDilate4.png", dilated);
+	imwrite("../../Assigment_1/FullCeils/Tv7FDilate4.png", dilated);
 
 	// rozdiel
 	Mat diff;
 	bitwise_xor(opening, dilated, diff);
-	imwrite("../../Assigment_1/Tv7FDiff.png", diff);
+	imwrite("../../Assigment_1/FullCeils/Tv7FDiff.png", diff);
 
-	// hladanie contur
+	// konturova analyza
 	vector<vector<Point> > finalContours;
 	findContours(diff, finalContours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(0, 0));
 
@@ -84,6 +86,7 @@ int main(int argc, char** argv)
 	int index = 0;
 	for (size_t i = 0; i < finalContours.size(); i++)
 	{
+		// podmienky na maximalnu a minimalnu sirku a vysku
 		if (minEllipse[i].size.height < 170 && minEllipse[i].size.width < 170
 			&& minEllipse[i].size.height > 50 && minEllipse[i].size.width > 50)
 		{
@@ -98,35 +101,52 @@ int main(int argc, char** argv)
 		}
 	}
 
+	// vypis poctu buniek
 	printf("Ceil count: %d\n", index);\
 	// Ceil count: 48
-	// Ceil count: 
+	// Ceil count: 274
 
-	imwrite("../../Assigment_1/Tv7FOutput.png", drawing);
+	// ulozenie vysledneho obrazka
+	imwrite("../../Assigment_1/FullCeils/Tv7FOutput.png", drawing);
 
+	// nacitanie filtracnych jadier
 	Mat kernel = imread("../../Assigment_1/kernel.png", IMREAD_GRAYSCALE);
 	Mat kernel2 = imread("../../Assigment_1/kernel2.png", IMREAD_GRAYSCALE);
 	threshold(kernel, kernel, 0, 255, THRESH_BINARY);
 	threshold(kernel2, kernel2, 0, 255, THRESH_BINARY);
+	imwrite("../../Assigment_1/FullCeils/Tv7FKernel1.png", kernel);
+	imwrite("../../Assigment_1/FullCeils/Tv7FKernel2.png", kernel2);
 
+	// convolucia prvym filtrom
 	Mat filtered;
 	filter2D(image, filtered, CV_32FC1, kernel);
 	normalize(filtered, filtered, 0, 1, cv::NORM_MINMAX);
+	imwrite("../../Assigment_1/FullCeils/Tv7FConv1.png", filtered);
 
+	// convolucia druhym filtrom
 	Mat filtered2;
 	filter2D(image, filtered2, CV_32FC1, kernel2);
 	normalize(filtered2, filtered2, 0, 1, cv::NORM_MINMAX);
+	imwrite("../../Assigment_1/FullCeils/Tv7FConv2.png", filtered2);
 
+	// rozmazanie pomocou gausovho filtra
 	Mat blur;
 	GaussianBlur(drawing, blur, Size(3, 3), 0);
+	imwrite("../../Assigment_1/FullCeils/Tv7FGaus.png", blur);
 
+	// kontury pomocou canny algoritmu
 	Mat canny;
 	Canny(drawing, canny, 10, 30);
+	imwrite("../../Assigment_1/FullCeils/Tv7FCanny.png", canny);
 
+	// vynatie hran pomocou sobelovho filtra
 	Mat sobelx, sobely, sobelxy;
 	Sobel(drawing, sobelx, CV_64F, 1, 0, 5);
 	Sobel(drawing, sobely, CV_64F, 0, 1, 5);
 	Sobel(drawing, sobelxy, CV_64F, 1, 1, 5);
+	imwrite("../../Assigment_1/FullCeils/Tv7FSobelx.png", sobelx);
+	imwrite("../../Assigment_1/FullCeils/Tv7FSobely.png", sobely);
+	imwrite("../../Assigment_1/FullCeils/Tv7FSobelxy.png", sobelxy);
 
 	waitKey();
 	return 0;
